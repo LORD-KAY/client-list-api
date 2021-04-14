d3589accb451edf06c389cada81d1112cb6d5552
Lord Kay <offeilord@gmail.com>
2021-04-14 17:30:44 +0000

    add post route to the clients route file

--- src/routes/clients.route.ts
+++ src/routes/clients.route.ts
@@ -1,8 +1,12 @@
-import express from "express";
+import * as express from "express";
+import { ClientPolicy } from "src/policies/validation.policy";
 import clientController from "../controllers/clients.controller";
 const router = express.Router();
 
 export default () => {
-  router.route(`/clients`).get(clientController.list);
+  router
+    .route(`/clients`)
+    .get(clientController.list)
+    .post(ClientPolicy.create, clientController.create);
   return router;
 };
