6fdf9dfc88c1a2dd63909f3a61b1789dcf41c31e
Lord Kay <offeilord@gmail.com>
2021-04-14 17:29:32 +0000

    Add create and update endpoint of clients operations to the clients controller

--- src/controllers/clients.controller.ts
+++ src/controllers/clients.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import Clients from "src/models/clients.schema";
+import { slugify } from "src/utils/helpers";
 
 const clientController = {
   async list(req: Request, res: Response) {
@@ -7,6 +9,68 @@ const clientController = {
       return res.status(500).json({
         message: `Unable to get list of clients`,
         path: req.path,
+        success: false,
+        timestamp: new Date().toISOString(),
+      });
+    }
+  },
+  async create(req: Request, res: Response) {
+    try {
+      const { email, name } = req.body;
+      const exists = await Clients.exists({ email });
+      if (!exists) {
+        const response = await Clients.create({
+          ...req.body,
+          slug: slugify(name),
+        });
+        return res.status(201).json({
+          message: `Client created successfully`,
+          success: false,
+          data: response,
+        });
+      }
+      return res.status(409).json({
+        message: `Client with the same email already exists`,
+        success: false,
+        timestamp: new Date().toISOString(),
+        path: req.path,
+      });
+    } catch (e) {
+      return res.status(500).json({
+        message: `Unable to create a new client`,
+        path: req.path,
+        success: false,
+        timestamp: new Date().toISOString(),
+      });
+    }
+  },
+  async update(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const { name } = req.body;
+      const exists = await Clients.exists({ _id: id });
+      if (!exists) {
+        return res.status(409).json({
+          message: `Client doesn't exist`,
+          success: false,
+          timestamp: new Date().toISOString(),
+          path: req.path,
+        });
+      }
+      const response = await Clients.updateOne(
+        { _id: id },
+        { ...req.body, slug: slugify(name) }
+      );
+      return res.status(201).json({
+        message: `Client created successfully`,
+        success: false,
+        data: response,
+      });
+    } catch (e) {
+      return res.status(500).json({
+        message: `Unable to create a new client`,
+        path: req.path,
+        success: false,
         timestamp: new Date().toISOString(),
       });
     }
